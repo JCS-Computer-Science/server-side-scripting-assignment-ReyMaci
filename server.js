bf53e8fb105f4d723d9925437844ca15806f3b77
Rey Maci <27262011@edu.cbe.ab.ca>
2024-10-23 15:25:38 -0600

    started the project

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ const express = require("express");
 const uuid = require("uuid")
 const server = express();
 server.use(express.json())
-
+server.use(express.static("public"))
 
 //All your code goes here
 let activeSessions={}
 
-//Do not remove this line. This allows the test suite to start
-//multiple instances of your server on different ports
-module.exports = server;
+server.get('/newgame', (req,res)=>{
+    let newID = uuld.v4()
+    let newgame = {
+        wordToGuess: "apple",
+        guesses:[],
+        wrongLetters: [],
+        closeLetters: [], //'a' is no longer close because it has been guessed in the correct spot
+        rightLetters: [],
+        remainingGuesses: 6,
+        gameOver: false
+    }
+})
+    
+    activeSession[newID] = newGame;
+    res.status(201);
+    res.send({sessionId: newID});
+    //Do not remove this line. This allows the test suite to start
+    //multiple instances of your server on different ports
+    server.get('/gamestate', (req,res) => {
+            
+    });
+    module.exports = server;

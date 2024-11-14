a98ddc9209f6594ae5c39ddf213f2850473f4bac
rey <reym1@educbe.ca>
2024-11-13 22:45:24 -0700

    worked during the break on finshing get/newgame,get/gamestate/GET/newgamewith answer,both posts and the last gets for returns 404 code and post for reutrn 400 code

--- server.js
+++ server.js
@@ -1,83 +1,204 @@
 const express = require("express");
-const uuid = require("uuid");
+const { errorMonitor } = require("supertest/lib/test");
+const uuid = require("uuid")
 const server = express();
-server.use(express.json());
-server.use(express.static("public"));
-
-let activeSessions = {};
+server.use(express.json())
+server.use(express.static('public'))
 
 
-server.get('/newgame', (req, res) => {
-    let newID = uuid.v4();
-    let newGame = {
-        wordToGuess: "apple",
-        guesses: [],
+let activeSessions={
+
+
+}
+
+
+server.get('/newgame',function(req,res){
+    let newID = uuid.v4()
+    let word = ""
+    if(req.query.answer == undefined){
+        word="stink"
+    } else{
+        word = req.query.answer
+    }
+   
+    let newgame = {
+        wordToGuess: word,
+        guesses:[],
         wrongLetters: [],
         closeLetters: [],
         rightLetters: [],
         remainingGuesses: 6,
         gameOver: false
-    };
-    activeSessions[newID] = newGame;
-    res.status(201).send({ sessionID: newID });
-});
-
-
-server.post('/guess', (req, res) => {
-    const { sessionID, letter } = req.body;
-
-    
-    const game = activeSessions[sessionID];
-    if (!game) {
-        return res.status(404).send({ error: "Session not found" });
     }
+    activeSessions[newID]= newgame
 
-  
-    if (game.guesses.includes(letter) || game.gameOver) {
-        return res.status(400).send({ error: "Letter has already been guessed or game is over" });
+
+
+
+//     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${newgame.wordToGuess}`;
+//     fetch(apiUrl)
+//     .then(response => {
+//     if (!response.ok) {
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     if(data.title != undefined){
+//         nuhuh()
+//     }
+//   })
+//     function nuhuh(){
+//         console.log("hello?")
+//         res.status(400)
+//         res.send({error: "Invalid Word"})
+//     }
+
+
+    res.status(201)
+    res.send({sessionID: newID})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.get('/gamestate', function(req,res){
+    sessionID = req.query.sessionID
+    if(sessionID == undefined){
+        res.status(400)
+        res.send({error: "Bad request"})
     }
-
-   
-    game.guesses.push(letter);
-
- 
-    if (game.wordToGuess.includes(letter)) {
-        game.rightLetters.push(letter);
-    } else {
-        game.wrongLetters.push(letter);
-        game.remainingGuesses -= 1;
+    gameState = activeSessions[sessionID]
+    if(gameState == undefined){
+        res.status(404)
+        res.send({error: "Failed"})
     }
+    res.status(200)
+    res.send({gameState: gameState})
+})
 
-    
-    if (!game.rightLetters.includes(letter) && !game.wrongLetters.includes(letter)) {
-        if (game.wordToGuess.split('').some((char) => char === letter)) {
-            game.closeLetters.push(letter);
+
+server.post('/guess', function(req,res){
+    sessionID = req.body.sessionID
+    if(sessionID == undefined){
+        res.status(400)
+        res.send({error: "Bad request"})
+    }
+    guess = req.body.guess.toLowerCase()
+    gameState = activeSessions[sessionID]
+    if(gameState == undefined){
+        res.status(404)
+        res.send({error: "Failed"})
+    }
+    let gstatus = [false,false,false,false,false]
+    if(gameState.remainingGuesses > 0 && gameState.gameOver != true){
+        let answer = gameState.wordToGuess.split('')
+        let attempt = guess.split('')
+
+
+        if(attempt.length != 5){
+            res.status(400)
+            res.send({error: "Invalid length"})
         }
+        let valid = /[a-z]/
+        for(let i=0;i<5;i++){
+            if(attempt[i].match(valid) == null){
+                res.status(400)
+                res.send({error: "Invalid Character"})
+            }
+        }
+        let newguess = [{value:attempt[0], result:''},{value:attempt[1], result:''},{value:attempt[2], result:''},{value:attempt[3], result:''},{value:attempt[4], result:''},]
+            for(let i=0; i <5;i++){
+                if(answer[i] == attempt[i]){
+                    if(gameState.rightLetters.includes(answer[i]) == false){
+                        if(gameState.closeLetters.includes(attempt[i]) == false){
+                            gameState.rightLetters.push(answer[i])
+                        } else {
+                            gameState.closeLetters.splice(gameState.closeLetters.indexOf(attempt[i]), 1)
+                            gameState.rightLetters.push(answer[i])
+                        }
+                    }
+                    newguess[i].result = 'RIGHT'
+                    gstatus[i] = true
+                } else {    
+                    for(let i2 = 0; i2<5;i2++){
+                        if(answer[i2] == attempt[i]){
+                            if(gstatus[i2] == false){
+                                if(gameState.closeLetters.includes(attempt[i]) == false){
+                                    if(gameState.rightLetters.includes(answer[i2]) == false) {
+                                        gameState.closeLetters.push(attempt[i])
+                                    }
+                                }
+                                newguess[i].result = 'CLOSE'
+                        }
+                    } else if(newguess[i].result != 'CLOSE'){
+                        newguess[i].result = 'WRONG'
+                    }
+                }
+                if(gameState.wrongLetters.includes(newguess[i].value) == false){
+                    if(gameState.closeLetters.includes(newguess[i].value) == false){
+                        gameState.wrongLetters.push(newguess[i].value)
+                    }
+                }
+            }  
     }
-
-    
-    if (game.remainingGuesses <= 0) {
-        game.gameOver = true;
+        gameState.remainingGuesses = gameState.remainingGuesses -1
+        gameState.guesses.push(newguess)
     }
-
-    
-    const response = {
-        wordToGuess: game.wordToGuess,
-        guesses: game.guesses,
-        wrongLetters: game.wrongLetters,
-        closeLetters: game.closeLetters,
-        rightLetters: game.rightLetters,
-        remainingGuesses: game.remainingGuesses,
-        gameOver: game.gameOver
-    };
-
-    res.status(201).send(response);
-});
-
-
-server.get('/gamestate', (req, res) => {
-    
-});
+    if(gameState.remainingGuesses <= 0){
+        gameState.gameOver=true
+    }
+    let correct = true
+    for(let i=0; i<5;i++){
+       if(gameState.guesses[gameState.guesses.length-1][i].result == "WRONG" || gameState.guesses[gameState.guesses.length-1][i].result == "CLOSE"){
+        correct = false
+       }
+    }
+    if(correct == true){
+        gameState.gameOver = true
+    }
+    res.status(201)
+    res.send({gameState: gameState})
+})
 
 
+
+
+
+
+
+
+
+//Do not remove this line. This allows the test suite to start
+//multiple instances of your server on different ports
+module.exports = server;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Do not remove this line. This allows the test suite to start
+//multiple instances of your server on different ports
 module.exports = server;
